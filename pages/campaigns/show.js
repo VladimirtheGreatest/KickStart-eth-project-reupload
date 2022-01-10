a924411933d0837f23ac177a7db186efea5b29a4
Vladimir <vladkodoktorov@gmail.com>
2022-01-10 16:22:16 +0000

    New form, grid layout refreshing contract data

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-import { Card } from "semantic-ui-react";
+import { Card, Grid, Button } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
+import ContributeForm from "../../components/ContributeForm";
+import { Link } from "../../routes";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -10,6 +13,7 @@ class CampaignShow extends Component {
     const summary = await campaign.methods.getSummary().call();
 
     return {
+      address: props.query.address,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
@@ -35,6 +39,29 @@ class CampaignShow extends Component {
           "The manager created this campaign and can create requests to withdraw money",
         style: { overflowWrap: "break-word" },
       },
+      {
+        header: minimumContribution,
+        meta: "Minimum Contribution (wei)",
+        description:
+          "You must contribute at least this much wei to become contributor",
+      },
+      {
+        header: requestsCount,
+        meta: "Number of requests",
+        description:
+          "A request tries to witdraw money from the contract. Requests must be approved by approvers",
+      },
+      {
+        header: approversCount,
+        meta: "Number of approvers",
+        description:
+          "Number of people who have already donated to this campaign",
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance(ether)",
+        description: "The balance is how much money this campaign has left",
+      },
     ];
     return <Card.Group items={items} />;
   }
@@ -42,7 +69,20 @@ class CampaignShow extends Component {
     return (
       <Layout>
         <h3>Campaign Show</h3>
-        {this.renderCards()}
+        <Grid>
+          <Grid.Column width={10}>
+            {this.renderCards()}
+            <Link route={`/campaigns/${this.props.address}/requests`}>
+              <a>
+                <Button secondary>View Requests</Button>
+              </a>
+            </Link>
+          </Grid.Column>
+
+          <Grid.Column width={6}>
+            <ContributeForm address={this.props.address} />
+          </Grid.Column>
+        </Grid>
       </Layout>
     );
   }
--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const routes = require("next-routes")();
 
 routes
   .add("/campaigns/new", "/campaigns/new")
-  .add("/campaigns/:address", "/campaigns/show");
+  .add("/campaigns/:address", "/campaigns/show")
+  .add("/campaigns/:address/requests", "/campaigns/requests/index");
+//route                //in which directory next should find that page
 
 module.exports = routes;
